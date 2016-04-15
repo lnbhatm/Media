7158cf4123b7f69f781ebddb00441404367e6576
Girish Bhat M <girishmb@gmail.com>
2016-04-15 19:13:01 +0530

    media:adding icon to project

--- frmmain.Designer.vb
+++ frmmain.Designer.vb
@@ -22,6 +22,7 @@ Partial Class frmmain
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmmain))
         Me.btnexit = New System.Windows.Forms.Button()
         Me.lblsrc = New System.Windows.Forms.Label()
         Me.lbldst = New System.Windows.Forms.Label()
@@ -33,7 +34,7 @@ Partial Class frmmain
         'btnexit
         '
         Me.btnexit.Font = New System.Drawing.Font("Cambria", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnexit.Location = New System.Drawing.Point(434, 204)
+        Me.btnexit.Location = New System.Drawing.Point(450, 130)
         Me.btnexit.Name = "btnexit"
         Me.btnexit.Size = New System.Drawing.Size(75, 30)
         Me.btnexit.TabIndex = 0
@@ -43,41 +44,51 @@ Partial Class frmmain
         'lblsrc
         '
         Me.lblsrc.AutoSize = True
-        Me.lblsrc.Location = New System.Drawing.Point(44, 114)
+        Me.lblsrc.BackColor = System.Drawing.Color.Transparent
+        Me.lblsrc.Font = New System.Drawing.Font("Cambria", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblsrc.ForeColor = System.Drawing.Color.Red
+        Me.lblsrc.Location = New System.Drawing.Point(8, 22)
         Me.lblsrc.Name = "lblsrc"
-        Me.lblsrc.Size = New System.Drawing.Size(73, 13)
+        Me.lblsrc.Size = New System.Drawing.Size(103, 19)
         Me.lblsrc.TabIndex = 1
         Me.lblsrc.Text = "Media Source"
         '
         'lbldst
         '
         Me.lbldst.AutoSize = True
-        Me.lbldst.Location = New System.Drawing.Point(41, 148)
+        Me.lbldst.BackColor = System.Drawing.Color.Transparent
+        Me.lbldst.Font = New System.Drawing.Font("Cambria", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbldst.ForeColor = System.Drawing.Color.Red
+        Me.lbldst.Location = New System.Drawing.Point(9, 61)
         Me.lbldst.Name = "lbldst"
-        Me.lbldst.Size = New System.Drawing.Size(92, 13)
+        Me.lbldst.Size = New System.Drawing.Size(134, 19)
         Me.lbldst.TabIndex = 2
         Me.lbldst.Text = "Media Destination"
         '
         'txtsrc
         '
-        Me.txtsrc.Font = New System.Drawing.Font("Cambria", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtsrc.Location = New System.Drawing.Point(169, 106)
+        Me.txtsrc.BackColor = System.Drawing.SystemColors.InactiveBorder
+        Me.txtsrc.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtsrc.Font = New System.Drawing.Font("Cambria", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtsrc.Location = New System.Drawing.Point(150, 22)
         Me.txtsrc.Name = "txtsrc"
-        Me.txtsrc.Size = New System.Drawing.Size(242, 22)
+        Me.txtsrc.Size = New System.Drawing.Size(300, 18)
         Me.txtsrc.TabIndex = 3
         '
         'txtdst
         '
-        Me.txtdst.Font = New System.Drawing.Font("Cambria", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtdst.Location = New System.Drawing.Point(171, 145)
+        Me.txtdst.BackColor = System.Drawing.SystemColors.InactiveBorder
+        Me.txtdst.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtdst.Font = New System.Drawing.Font("Cambria", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtdst.Location = New System.Drawing.Point(150, 61)
         Me.txtdst.Name = "txtdst"
-        Me.txtdst.Size = New System.Drawing.Size(238, 22)
+        Me.txtdst.Size = New System.Drawing.Size(300, 18)
         Me.txtdst.TabIndex = 4
         '
         'btnorg
         '
         Me.btnorg.Font = New System.Drawing.Font("Cambria", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnorg.Location = New System.Drawing.Point(353, 204)
+        Me.btnorg.Location = New System.Drawing.Point(452, 93)
         Me.btnorg.Name = "btnorg"
         Me.btnorg.Size = New System.Drawing.Size(75, 31)
         Me.btnorg.TabIndex = 5
@@ -88,6 +99,7 @@ Partial Class frmmain
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.Media.My.Resources.Resources.DSCN1281
         Me.ClientSize = New System.Drawing.Size(535, 252)
         Me.Controls.Add(Me.btnorg)
         Me.Controls.Add(Me.txtdst)
@@ -96,7 +108,9 @@ Partial Class frmmain
         Me.Controls.Add(Me.lblsrc)
         Me.Controls.Add(Me.btnexit)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "frmmain"
+        Me.TransparencyKey = System.Drawing.Color.White
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
