255213a90f6d1a99aa3bcb8c4b6ccd399da7699b
Girish Bhat M <girishmb@gmail.com>
2016-04-15 18:44:08 +0530

    media: initial project creation

---
+++ frmmain.Designer.vb
@@ -0,0 +1,111 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmmain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnexit = New System.Windows.Forms.Button()
+        Me.lblsrc = New System.Windows.Forms.Label()
+        Me.lbldst = New System.Windows.Forms.Label()
+        Me.txtsrc = New System.Windows.Forms.TextBox()
+        Me.txtdst = New System.Windows.Forms.TextBox()
+        Me.btnorg = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnexit
+        '
+        Me.btnexit.Font = New System.Drawing.Font("Cambria", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnexit.Location = New System.Drawing.Point(434, 204)
+        Me.btnexit.Name = "btnexit"
+        Me.btnexit.Size = New System.Drawing.Size(75, 30)
+        Me.btnexit.TabIndex = 0
+        Me.btnexit.Text = "Exit"
+        Me.btnexit.UseVisualStyleBackColor = True
+        '
+        'lblsrc
+        '
+        Me.lblsrc.AutoSize = True
+        Me.lblsrc.Location = New System.Drawing.Point(44, 114)
+        Me.lblsrc.Name = "lblsrc"
+        Me.lblsrc.Size = New System.Drawing.Size(73, 13)
+        Me.lblsrc.TabIndex = 1
+        Me.lblsrc.Text = "Media Source"
+        '
+        'lbldst
+        '
+        Me.lbldst.AutoSize = True
+        Me.lbldst.Location = New System.Drawing.Point(41, 148)
+        Me.lbldst.Name = "lbldst"
+        Me.lbldst.Size = New System.Drawing.Size(92, 13)
+        Me.lbldst.TabIndex = 2
+        Me.lbldst.Text = "Media Destination"
+        '
+        'txtsrc
+        '
+        Me.txtsrc.Font = New System.Drawing.Font("Cambria", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtsrc.Location = New System.Drawing.Point(169, 106)
+        Me.txtsrc.Name = "txtsrc"
+        Me.txtsrc.Size = New System.Drawing.Size(242, 22)
+        Me.txtsrc.TabIndex = 3
+        '
+        'txtdst
+        '
+        Me.txtdst.Font = New System.Drawing.Font("Cambria", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtdst.Location = New System.Drawing.Point(171, 145)
+        Me.txtdst.Name = "txtdst"
+        Me.txtdst.Size = New System.Drawing.Size(238, 22)
+        Me.txtdst.TabIndex = 4
+        '
+        'btnorg
+        '
+        Me.btnorg.Font = New System.Drawing.Font("Cambria", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnorg.Location = New System.Drawing.Point(353, 204)
+        Me.btnorg.Name = "btnorg"
+        Me.btnorg.Size = New System.Drawing.Size(75, 31)
+        Me.btnorg.TabIndex = 5
+        Me.btnorg.Text = "Begin"
+        Me.btnorg.UseVisualStyleBackColor = True
+        '
+        'frmmain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(535, 252)
+        Me.Controls.Add(Me.btnorg)
+        Me.Controls.Add(Me.txtdst)
+        Me.Controls.Add(Me.txtsrc)
+        Me.Controls.Add(Me.lbldst)
+        Me.Controls.Add(Me.lblsrc)
+        Me.Controls.Add(Me.btnexit)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "frmmain"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnexit As System.Windows.Forms.Button
+    Friend WithEvents lblsrc As System.Windows.Forms.Label
+    Friend WithEvents lbldst As System.Windows.Forms.Label
+    Friend WithEvents txtsrc As System.Windows.Forms.TextBox
+    Friend WithEvents txtdst As System.Windows.Forms.TextBox
+    Friend WithEvents btnorg As System.Windows.Forms.Button
+
+End Class
---
+++ frmmain.vb
@@ -0,0 +1,74 @@
+﻿Public Class frmmain
+
+    Protected Overrides ReadOnly Property CreateParams() As CreateParams
+        Get
+            Dim param As CreateParams = MyBase.CreateParams
+            param.ClassStyle = param.ClassStyle Or &H200
+            Return param
+        End Get
+    End Property
+
+
+
+    Private Sub btnexit_Click(sender As Object, e As EventArgs) Handles btnexit.Click
+        If (System.Windows.Forms.Application.MessageLoop) Then
+            System.Windows.Forms.Application.Exit()
+        Else
+            System.Environment.Exit(1)
+        End If
+    End Sub
+
+    Private Sub frmmain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        txtsrc.Text = "C:\Users\bhatml\Desktop\docs\Camera"
+        txtdst.Text = "C:\Users\bhatml\Desktop\docs\Camera\dst"
+    End Sub
+
+
+   
+
+
+    Public Function FileExists(ByVal Fname As String) As Boolean
+        If My.Computer.FileSystem.FileExists(Fname) Then
+            FileExists = True
+        Else
+            FileExists = False
+        End If
+    End Function
+
+    Public Function DirectoryExists(ByVal dirname As String) As Boolean
+        If My.Computer.FileSystem.DirectoryExists(dirname) Then
+            DirectoryExists = True
+        Else
+            DirectoryExists = False
+        End If
+    End Function
+
+
+
+    Public Function makedirectory(ByVal dirname As String) As Boolean
+        makedirectory = False
+        Try
+            If My.Computer.FileSystem.DirectoryExists(dirname) = False Then
+                My.Computer.FileSystem.CreateDirectory(dirname)
+                makedirectory = True
+            End If
+        Catch ex As System.IO.IOException
+            MsgBox("An error occurred")
+            makedirectory = True
+        End Try
+    End Function
+
+    Private Sub btnorg_Click(sender As Object, e As EventArgs) Handles btnorg.Click
+        Dim strfiles As String = "Files: "
+
+        For Each foundFile As String In My.Computer.FileSystem.GetFiles(txtsrc.Text)
+
+            strfiles = strfiles & vbCrLf & foundFile
+
+        Next
+
+        MsgBox(strfiles)
+
+
+    End Sub
+End Class
